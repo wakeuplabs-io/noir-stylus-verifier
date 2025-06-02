26cab15fcbd0ca78dded56ab7c741e547c40c497
Matias Zapata <matuzapata@gmail.com>
2025-06-02 16:34:25 -0500

    remove mpc core

--- co-noir/co-brillig/src/mpc.rs
+++ co-noir/co-brillig/src/mpc.rs
@@ -10,16 +10,10 @@ use std::fmt;
 use ark_ff::PrimeField;
 
 mod plain;
-mod rep3;
-mod shamir;
 use brillig::{BitSize, IntegerBitSize};
 pub use plain::PlainBrilligDriver;
-pub use rep3::Rep3BrilligDriver;
-pub use shamir::ShamirBrilligDriver;
 
 pub use plain::PlainBrilligType;
-pub use rep3::Rep3BrilligType;
-pub use shamir::ShamirBrilligType;
 
 const CHAR_BIT_SIZE: IntegerBitSize = IntegerBitSize::U8;
 
