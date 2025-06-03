e86bc8ae9c38743e0817e99e520ad7e6e9118e41
Matias Zapata <matuzapata@gmail.com>
2025-06-03 12:59:04 -0500

    remove oink prover

--- co-noir/ultrahonk/src/oink/verifier.rs
+++ co-noir/ultrahonk/src/oink/verifier.rs
@@ -1,11 +1,93 @@
 use super::types::VerifierMemory;
+use crate::verifier::HonkVerifyResult;
 use crate::{
-    oink::prover::Oink,
-    builder::TranscriptFieldType,
-    transcript::{Transcript, TranscriptHasher},
-    verifier::HonkVerifyResult,
+    transcript::{Transcript, TranscriptFieldType, TranscriptHasher},
+    NUM_ALPHAS,
 };
+use ark_ff::One;
 use co_builder::prelude::{HonkCurve, VerifyingKey};
+use std::{array, marker::PhantomData};
+
+pub(crate) struct Oink<P: HonkCurve<TranscriptFieldType>, H: TranscriptHasher<TranscriptFieldType>>
+{
+    phantom_data: PhantomData<P>,
+    phantom_hasher: PhantomData<H>,
+}
+
+impl<P: HonkCurve<TranscriptFieldType>, H: TranscriptHasher<TranscriptFieldType>> Default
+    for Oink<P, H>
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<P: HonkCurve<TranscriptFieldType>, H: TranscriptHasher<TranscriptFieldType>> Oink<P, H> {
+    pub(crate) fn new() -> Self {
+        Self {
+            phantom_data: PhantomData,
+            phantom_hasher: PhantomData,
+        }
+    }
+
+    pub(crate) fn compute_public_input_delta(
+        beta: &P::ScalarField,
+        gamma: &P::ScalarField,
+        public_inputs: &[P::ScalarField],
+        circuit_size: u32,
+        pub_inputs_offset: u32,
+    ) -> P::ScalarField {
+        tracing::trace!("compute public input delta");
+
+        // Let m be the number of public inputs x₀,…, xₘ₋₁.
+        // Recall that we broke the permutation σ⁰ by changing the mapping
+        //  (i) -> (n+i)   to   (i) -> (-(i+1))   i.e. σ⁰ᵢ = −(i+1)
+        //
+        // Therefore, the term in the numerator with ID¹ᵢ = n+i does not cancel out with any term in the denominator.
+        // Similarly, the denominator contains an extra σ⁰ᵢ = −(i+1) term that does not appear in the numerator.
+        // We expect the values of W⁰ᵢ and W¹ᵢ to be equal to xᵢ.
+        // The expected accumulated product would therefore be equal to
+
+        //   ∏ᵢ (γ + W¹ᵢ + β⋅ID¹ᵢ)        ∏ᵢ (γ + xᵢ + β⋅(n+i) )
+        //  -----------------------  =  ------------------------
+        //   ∏ᵢ (γ + W⁰ᵢ + β⋅σ⁰ᵢ )        ∏ᵢ (γ + xᵢ - β⋅(i+1) )
+
+        // At the start of the loop for each xᵢ where i = 0, 1, …, m-1,
+        // we have
+        //      numerator_acc   = γ + β⋅(n+i) = γ + β⋅n + β⋅i
+        //      denominator_acc = γ - β⋅(1+i) = γ - β   - β⋅i
+        // at the end of the loop, add and subtract β to each term respectively to
+        // set the expected value for the start of iteration i+1.
+        // Note: The public inputs may be offset from the 0th index of the wires, for example due to the inclusion of an
+        // initial zero row or Goblin-stlye ECC op gates. Accordingly, the indices i in the above formulas are given by i =
+        // [0, m-1] + offset, i.e. i = offset, 1 + offset, …, m - 1 + offset.
+
+        let mut num = P::ScalarField::one();
+        let mut denom = P::ScalarField::one();
+        let mut num_acc =
+            *gamma + P::ScalarField::from((circuit_size + pub_inputs_offset) as u64) * beta;
+        let mut denom_acc = *gamma - P::ScalarField::from((1 + pub_inputs_offset) as u64) * beta;
+
+        for x_i in public_inputs.iter() {
+            num *= num_acc + x_i;
+            denom *= denom_acc + x_i;
+            num_acc += beta;
+            denom_acc -= beta;
+        }
+        num / denom
+    }
+
+    /// Generate relation separators alphas for sumcheck/combiner computation
+    pub(crate) fn generate_alphas_round(
+        alphas: &mut [P::ScalarField; NUM_ALPHAS],
+        transcript: &mut Transcript<TranscriptFieldType, H>,
+    ) {
+        tracing::trace!("generate alpha round");
+
+        let args: [String; NUM_ALPHAS] = array::from_fn(|i| format!("alpha_{}", i));
+        alphas.copy_from_slice(&transcript.get_challenges::<P>(&args));
+    }
+}
 
 pub(crate) struct OinkVerifier<
     P: HonkCurve<TranscriptFieldType>,
