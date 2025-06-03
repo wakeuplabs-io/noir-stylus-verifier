cda338af31c7c1925555f9d42fb00b2723cab973
Matias Zapata <matuzapata@gmail.com>
2025-06-03 12:32:04 -0500

    adapt test to only work for verifier so we can further cleanup code

--- co-noir/ultrahonk/tests/plain.rs
+++ co-noir/ultrahonk/tests/plain.rs
@@ -1,97 +1,89 @@
 use ark_bn254::Bn254;
 use co_builder::prelude::CrsParser;
-use co_builder::prelude::HonkRecursion;
+use co_builder::prelude::Serialize as FieldSerialize;
+use co_builder::prelude::VerifyingKeyBarretenberg;
 use co_builder::prelude::ZeroKnowledge;
 use sha3::Keccak256;
+use ultrahonk::builder::VerifyingKey;
 use ultrahonk::{
-    prelude::{
-        HonkProof, PlainAcvmSolver, Poseidon2Sponge, TranscriptFieldType, TranscriptHasher,
-        UltraCircuitBuilder, UltraHonk,
-    },
-    Utils,
+    builder::{TranscriptFieldType},
+    prelude::{HonkProof, TranscriptHasher, UltraHonk},
 };
 
 fn plain_test<H: TranscriptHasher<TranscriptFieldType>>(
     proof_file: &str,
-    circuit_file: &str,
-    witness_file: &str,
-    has_zk: ZeroKnowledge,
+    vk_file: &str,
+    public_inputs_file: &str,
 ) {
-    const CRS_PATH_G1: &str = "../co-builder/src/crs/bn254_g1.dat";
     const CRS_PATH_G2: &str = "../co-builder/src/crs/bn254_g2.dat";
 
-    let constraint_system = Utils::get_constraint_system_from_file(circuit_file, true).unwrap();
+    // parse proof file
+    let proof_u8 = std::fs::read(&proof_file).unwrap();
+    let proof = HonkProof::from_buffer(&proof_u8).unwrap();
 
-    let witness = Utils::get_witness_from_file(witness_file).unwrap();
-    let mut driver = PlainAcvmSolver::new();
-    let builder = UltraCircuitBuilder::<Bn254>::create_circuit(
-        &constraint_system,
-        false, // We don't support recursive atm
-        0,
-        witness,
-        HonkRecursion::UltraHonk,
-        &mut driver,
-    )
-    .unwrap();
-    let crs_size = builder.compute_dyadic_size();
-    let crs = CrsParser::get_crs(CRS_PATH_G1, CRS_PATH_G2, crs_size, has_zk).unwrap();
-    let (prover_crs, verififer_crs) = crs.split();
+    // parse public_inputs file
+    let public_inputs_u8 = std::fs::read(&public_inputs_file).unwrap();
+    let public_inputs = FieldSerialize::from_buffer(&public_inputs_u8, false).unwrap();
 
-    let (proving_key, verifying_key) = builder
-        .create_keys(prover_crs.into(), verififer_crs, &mut driver)
-        .unwrap();
+    // parse the crs
+    let verifier_crs = CrsParser::<Bn254>::get_crs_g2(CRS_PATH_G2).unwrap();
 
-    let (proof, public_inputs) = UltraHonk::<_, H>::prove(proving_key, has_zk).unwrap();
-    if has_zk == ZeroKnowledge::No {
-        let proof_u8 = proof.to_buffer();
-        let read_proof_u8 = std::fs::read(proof_file).unwrap();
-        assert_eq!(proof_u8, read_proof_u8);
-
-        let read_proof = HonkProof::from_buffer(&read_proof_u8).unwrap();
-        assert_eq!(proof, read_proof);
-    }
+    // parse verification key file
+    let vk_u8 = std::fs::read(&vk_file).unwrap();
+    let vk = VerifyingKeyBarretenberg::<Bn254>::from_buffer(&vk_u8).unwrap();
+    let vk = VerifyingKey::from_barrettenberg_and_crs(vk, verifier_crs);
 
     let is_valid =
-        UltraHonk::<_, H>::verify(proof, &public_inputs, &verifying_key, has_zk).unwrap();
+        UltraHonk::<_, H>::verify(proof, &public_inputs, &vk, ZeroKnowledge::No)
+            .unwrap();
     assert!(is_valid);
-}
 
-#[test]
-fn poseidon_test_poseidon2sponge() {
-    const PROOF_FILE: &str = "../../test_vectors/noir/poseidon/kat/pos_proof_with_pos";
-    const CIRCUIT_FILE: &str = "../../test_vectors/noir/poseidon/kat/poseidon.json";
-    const WITNESS_FILE: &str = "../../test_vectors/noir/poseidon/kat/poseidon.gz";
-
-    plain_test::<Poseidon2Sponge>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::No);
-    plain_test::<Poseidon2Sponge>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::Yes);
 }
 
 #[test]
 fn poseidon_test_keccak256() {
-    const PROOF_FILE: &str = "../../test_vectors/noir/poseidon/kat/pos_proof_with_kec";
-    const CIRCUIT_FILE: &str = "../../test_vectors/noir/poseidon/kat/poseidon.json";
-    const WITNESS_FILE: &str = "../../test_vectors/noir/poseidon/kat/poseidon.gz";
+    const PROOF_FILE: &str = "../../test_vectors/poseidon/kat/proof";
+    const VK_FILE: &str = "../../test_vectors/poseidon/kat/vk";
+    const PUBLIC_INPUTS_FILE: &str = "../../test_vectors/poseidon/kat/public_inputs";
 
-    plain_test::<Keccak256>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::No);
-    plain_test::<Keccak256>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::Yes);
+    plain_test::<Keccak256>(PROOF_FILE, VK_FILE, PUBLIC_INPUTS_FILE);
 }
 
 #[test]
 fn add3_test_keccak256() {
-    const PROOF_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64_proof_with_kec";
-    const CIRCUIT_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64.json";
-    const WITNESS_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64.gz";
+    const PROOF_FILE: &str = "../../test_vectors/add3u64/kat/add3u64_proof_with_kec";
+    const VK_FILE: &str = "../../test_vectors/add3u64/kat/add3u64.vk";
+    const PUBLIC_INPUTS_FILE: &str = "../../test_vectors/add3u64/kat/add3u64.public_inputs";
 
-    plain_test::<Keccak256>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::No);
-    plain_test::<Keccak256>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::Yes);
+    plain_test::<Keccak256>(PROOF_FILE, VK_FILE, PUBLIC_INPUTS_FILE);
 }
 
-#[test]
-fn add3_test_poseidon2sponge() {
-    const PROOF_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64_proof_with_pos";
-    const CIRCUIT_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64.json";
-    const WITNESS_FILE: &str = "../../test_vectors/noir/add3u64/kat/add3u64.gz";
 
-    plain_test::<Poseidon2Sponge>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::No);
-    plain_test::<Poseidon2Sponge>(PROOF_FILE, CIRCUIT_FILE, WITNESS_FILE, ZeroKnowledge::Yes);
-}
+// #[test]
+// fn poseidon_test_keccak256() {
+//     const PROOF_FILE: &str = "../../test_vectors/noir/poseidon/target/proof";
+//     const VK_FILE: &str = "../../test_vectors/noir/poseidon/target/vk";
+//     const PUBLIC_INPUTS_FILE: &str = "../../test_vectors/noir/poseidon/target/public_inputs";
+//     const CRS_PATH_G2: &str = "../../test_vectors/noir/poseidon/bn254_g2.dat";
+
+//     // parse proof file
+//     let proof_u8 = std::fs::read(&PROOF_FILE).unwrap();
+//     let proof = HonkProof::from_buffer(&proof_u8).unwrap();
+
+//     // parse public_inputs file
+//     let public_inputs_u8 = std::fs::read(&PUBLIC_INPUTS_FILE).unwrap();
+//     let public_inputs = FieldSerialize::from_buffer(&public_inputs_u8, false).unwrap();
+
+//     // parse the crs
+//     let verifier_crs = CrsParser::<Bn254>::get_crs_g2(CRS_PATH_G2).unwrap();
+
+//     // parse verification key file
+//     let vk_u8 = std::fs::read(&VK_FILE).unwrap();
+//     let vk = VerifyingKeyBarretenberg::<Bn254>::from_buffer(&vk_u8).unwrap();
+//     let vk = VerifyingKey::from_barrettenberg_and_crs(vk, verifier_crs);
+
+//     let is_valid =
+//         UltraHonk::<_, Keccak256>::verify(proof, &public_inputs, &vk, ZeroKnowledge::No)
+//             .unwrap();
+//     assert!(is_valid);
+// }
