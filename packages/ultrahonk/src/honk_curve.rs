2cdaf6d7e5cc35ae3f95368597c8e6ffa8960299
Matias Zapata <matuzapata@gmail.com>
2025-06-18 12:15:37 -0600

    cleanup code

--- packages/ultrahonk/src/honk_curve.rs
+++ packages/ultrahonk/src/honk_curve.rs
@@ -12,16 +12,6 @@ pub const NUM_BASEFIELD_ELEMENTS: usize = 2;
 /// This is the number of elements required to represent the scalar field.
 pub const NUM_SCALARFIELD_ELEMENTS: usize = 1;
 
-/// The size of the subgroup in the ultrahonk::HonkCurve representation.
-///
-/// This is the order of the subgroup of G1.
-pub const SUBGROUP_SIZE: usize = 256;
-
-/// The number of univariate polynomials in the ultrahonk::HonkCurve representation used by Libra.
-///
-/// This is the number of polynomials required to encode the Libra VM's state.
-pub const LIBRA_UNIVARIATES_LENGTH: usize = 9;
-
 // TODO: improve this, make constants instead
 pub trait HonkCurve: G1ArithmeticBackend {
     fn get_curve_b() -> ScalarField {
