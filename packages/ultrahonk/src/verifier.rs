bb1bc2a47f39df8f46ad4633264a451584dc5f4c
Matias Zapata <matuzapata@gmail.com>
2025-07-08 11:05:56 -0600

    cleanup

--- packages/ultrahonk/src/verifier.rs
+++ packages/ultrahonk/src/verifier.rs
@@ -23,21 +23,11 @@ impl UltraHonk {
 
         let mut transcript = Transcript::new_verifier(honk_proof);
         let memory = VerifierMemory::from_key_and_transcript::<H>(vk, &mut transcript);
-        // let memory_bytes = memory.serialize_to_bytes();
 
         let mut decider_verifier = DeciderVerifier::new(memory);
         let sumcheck_output =
             decider_verifier.verify_sumcheck::<H>(&mut transcript, vk.circuit_size)?;
-
-        transcript =
-            Transcript::deserialize_from_bytes(transcript.serialize_to_bytes().as_slice()).unwrap();
-        let memory = VerifierMemory::deserialize_from_bytes(
-            decider_verifier.memory.serialize_to_bytes().as_slice(),
-        )
-        .unwrap();
-        let mut decider_verifier2 = DeciderVerifier::new(memory);
-
-        let shplemini_output = decider_verifier2.verify_shplemini::<H, P>(
+        let shplemini_output = decider_verifier.verify_shplemini::<H, P>(
             &mut transcript,
             sumcheck_output.multivariate_challenge,
             vk.circuit_size,
