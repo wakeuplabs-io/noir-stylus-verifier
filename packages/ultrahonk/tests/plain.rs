0ed7b5bd003387e1c39f3bd9aadea4ad58647c2b
Matias Zapata <matuzapata@gmail.com>
2025-07-08 10:58:40 -0600

    cleanup

--- packages/ultrahonk/tests/plain.rs
+++ packages/ultrahonk/tests/plain.rs
@@ -1,21 +1,16 @@
 #![macro_use]
 extern crate alloc;
 
-use std::process::Command;
-
 use alloc::vec::Vec;
 use ark_bn254::Bn254;
-use ark_ff::Zero;
 use ark_ec::{pairing::Pairing, AffineRepr, CurveGroup};
 use ark_ff::One;
 use sha3::{Digest, Keccak256};
-use ultrahonk::constants::NUM_BYTES_FELT;
-use ultrahonk::types::G1BaseField;
 use ultrahonk::{
     backends::{G1ArithmeticBackend, G1ArithmeticError, HashBackend},
     constants::HASH_OUTPUT_SIZE,
     keys::verification_key::VerifyingKey,
-    serialize::{BytesDeserializable, BytesSerializable},
+    serialize::BytesDeserializable,
     types::{G1Affine, G2Affine, HonkProof, ScalarField},
     verifier::UltraHonk,
 };
@@ -37,79 +32,14 @@ pub struct ArkHonkCurve;
 impl G1ArithmeticBackend for ArkHonkCurve {
     /// Add two points in G1
     fn ec_add(a: G1Affine, b: G1Affine) -> Result<G1Affine, G1ArithmeticError> {
-        if a == G1Affine::identity() {
-            return Ok(b);
-        } else if b == G1Affine::identity() {
-            return Ok(a);
-        }
-
-        // Serialize the points
-        let mut calldata = [0_u8; NUM_BYTES_FELT * 4];
-        calldata[..NUM_BYTES_FELT * 2].copy_from_slice(&serialize_g1_affine_to_bytes(a));
-        calldata[NUM_BYTES_FELT * 2..].copy_from_slice(&serialize_g1_affine_to_bytes(b));
-
-        // println!("cast call {} \"0x{}\"", "0x0000000000000000000000000000000000000006", hex::encode(calldata));
-
-        let output = Command::new("cast")
-            .args(["call", "0x0000000000000000000000000000000000000006", &format!("0x{}", hex::encode(calldata))])
-            .output()
-            .expect("Failed to execute cast");
-
-        if !output.status.success() {
-            eprintln!(
-                "Cast call failed: {}",
-                String::from_utf8_lossy(&output.stderr)
-            );
-            return Err(G1ArithmeticError);
-        } else  {
-            let stdout = String::from_utf8_lossy(&output.stdout);   
-            let call_result = stdout.split("0x").nth(1).unwrap().trim();
-            let out = hex::decode(call_result).unwrap();
-
-            // let result = deserialize_from_bytes(&out);
-            let result = G1Affine::deserialize_from_bytes(&out).unwrap();
-           return Ok(result)
-        } 
-
-        // Ok((a + b).into_affine())
+        Ok((a + b).into_affine())
     }
 
     /// Multiply a G1 point by a scalar in its scalar field
     fn ec_scalar_mul(a: ScalarField, b: G1Affine) -> Result<G1Affine, G1ArithmeticError> {
         let mut b_group = b.into_group();
         b_group *= a;
-        // Ok(b_group.into_affine())
-
-
-        if a == ScalarField::one() {
-            return Ok(b);
-        }
-
-        // Serialize the point and scalar
-        let mut calldata = [0_u8; NUM_BYTES_FELT * 3];
-        calldata[..NUM_BYTES_FELT * 2].copy_from_slice(&b.serialize_to_bytes());
-        calldata[NUM_BYTES_FELT * 2..].copy_from_slice(&a.serialize_to_bytes());
-
-        let output = Command::new("cast")
-            .args(["call", "0x0000000000000000000000000000000000000007", &format!("0x{}", hex::encode(calldata))])
-            .output()
-            .expect("Failed to execute cast");
-        println!("cast call 0x0000000000000000000000000000000000000007 \"0x{}\"", hex::encode(calldata));
-
-        if !output.status.success() {
-            eprintln!("Cast call failed: {}", String::from_utf8_lossy(&output.stderr));
-            return Err(G1ArithmeticError);
-        } else {
-            let stdout = String::from_utf8_lossy(&output.stdout);   
-            let call_result = stdout.split("0x").nth(1).unwrap().trim();
-
-            println!("\nexpected: {:?}", hex::encode(b_group.into_affine().serialize_to_bytes()));
-            println!("call_result: {:?}\n", call_result);
-
-            let out = hex::decode(call_result).unwrap();
-            let result = G1Affine::deserialize_from_bytes(&out).unwrap();
-            return Ok(result);
-        }
+        Ok(b_group.into_affine())
     }
 
     /// Check the pairing identity e(a_1, b_1) == e(a_2, b_2)
@@ -132,9 +62,6 @@ impl G1ArithmeticBackend for ArkHonkCurve {
         if scalars.len() != points.len() {
             return Err(G1ArithmeticError);
         }
-
-        println!("scalars: {:?}", scalars.len());
-        println!("points: {:?}", points.len());
 
         scalars
             .iter()
@@ -172,8 +99,6 @@ fn test_iterating_test_vectors() {
     let test_vectors_dir = "../../test_vectors";
     let test_vectors = std::fs::read_dir(test_vectors_dir).unwrap();
 
-    println!("test_vectors_dir: {}", test_vectors_dir);
-
     for entry in test_vectors {
         let entry = entry.unwrap();
         let path = entry.path();
@@ -191,38 +116,5 @@ fn test_iterating_test_vectors() {
             &vk_file,
             &public_inputs_file,
         );
-
-        break;
     }
-}
-
-
-
-fn deserialize_cursor<D: BytesDeserializable>(
-    bytes: &[u8],
-    cursor: &mut usize,
-) -> D {
-    let elem = D::deserialize_from_bytes(&bytes[*cursor..*cursor + D::SER_LEN]).unwrap();
-    *cursor += D::SER_LEN;
-    elem
-}
-
-fn deserialize_g1_affine_from_bytes(bytes: &[u8]) -> G1Affine {
-    // Note: although this performs modular reduction, it's safe to do so
-    // since we can assume that precompiles will always correctly return
-    // elements contained in the field
-    let mut cursor = 0;
-    let x = G1BaseField::deserialize_from_bytes_with_offset(bytes, &mut cursor).unwrap();
-    let y = G1BaseField::deserialize_from_bytes_with_offset(bytes, &mut cursor).unwrap();
-
-    G1Affine { x, y, infinity: x.is_zero() && y.is_zero() }
-}
-
-fn serialize_g1_affine_to_bytes(d: G1Affine) -> Vec<u8> {
-    let zero = G1BaseField::zero();
-    let (x, y) = d.xy().unwrap_or((zero, zero));
-    let mut bytes = Vec::with_capacity(NUM_BYTES_FELT * 2);
-    bytes.extend(x.serialize_to_bytes());
-    bytes.extend(y.serialize_to_bytes());
-    bytes
 }
