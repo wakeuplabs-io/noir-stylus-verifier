0d5ba896fb8a53c21975e4f5efd6cf793bc9bfdf
Matias Zapata <matuzapata@gmail.com>
2025-08-11 10:18:41 -0600

    fix deployment from contracts package

--- contracts/src/main.rs
+++ contracts/src/main.rs
@@ -6,8 +6,20 @@ pub extern "C" fn main() {}
 
 #[cfg(feature = "export-abi")]
 fn main() {
-    use contracts::VerifierContract;
     use stylus_sdk::abi::export::print_abi;
 
-    print_abi::<VerifierContract>("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    #[cfg(any(feature = "verifier", feature = "zk-verifier"))] {
+        use contracts::contracts::core::verifier::VerifierContract;
+        print_abi::<VerifierContract>("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    }
+
+    #[cfg(any(feature = "sumcheck-verifier", feature = "zk-sumcheck-verifier"))] {
+        use contracts::contracts::core::sumcheck_verifier::SumcheckVerifierContract;
+        print_abi::<SumcheckVerifierContract>("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    }
+
+    #[cfg(any(feature = "shplemini-verifier", feature = "zk-shplemini-verifier"))] {
+        use contracts::contracts::core::shplemini_verifier::ShpleminiVerifierContract;
+        print_abi::<ShpleminiVerifierContract>("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    }
 }
